7c067a10a5b31ad2c6ae3e0c688dc94594869058
Tazzmanian <banskaliqtabg@yahoo.com>
2025-07-30 01:15:30 +0300

    complete customer service /for now/

--- Services/customer/src/main/java/org/services/Customer.java
+++ Services/customer/src/main/java/org/services/Customer.java
@@ -1,11 +1,16 @@
 package org.services;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String firstName;
     private String lastName;
--- Services/customer/src/main/java/org/services/CustomerService.java
+++ Services/customer/src/main/java/org/services/CustomerService.java
@@ -1,15 +1,18 @@
 package org.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+@Slf4j
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRequest customerRequest) {
         Customer customer = Customer.builder()
                 .email(customerRequest.email())
                 .firstName(customerRequest.firstName())
                 .lastName(customerRequest.lastName())
                 .build();
-
+        customer = customerRepository.save(customer);
+        log.info("customer registered: {}", customer);
     }
 }
