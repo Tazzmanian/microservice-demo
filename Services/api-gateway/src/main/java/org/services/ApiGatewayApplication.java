8eac9b289ef1c513702170ba5c31fd8223719e7d
Tazzmanian <banskaliqtabg@yahoo.com>
2025-08-03 23:01:30 +0300

    add api gateway

---
+++ Services/api-gateway/src/main/java/org/services/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package org.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ApiGatewayApplication
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
